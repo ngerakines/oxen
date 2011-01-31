d0f047652f1bc835b418e217c98317d33a0e02a1
Nick Gerakines <nick@gerakines.net>
2011-01-30 22:40:43 -0800

    first commit

---
+++ README.markdown
@@ -0,0 +1,15 @@
+# About
+
+# USAGE
+
+
+
+# TODO
+
+# License
+
+Copyright (c) 2011 Nick Gerakines
+
+Open sourced under the MIT license. See the included LICENSE file for more
+information.
+
---
+++ src/oxen.cpp
@@ -0,0 +1,47 @@
+/*
+** Copyright (c) 2011 Nick Gerakines
+** 
+** Permission is hereby granted, free of charge, to any person obtaining a copy
+** of this software and associated documentation files (the "Software"), to deal
+** in the Software without restriction, including without limitation the rights
+** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+** copies of the Software, and to permit persons to whom the Software is
+** furnished to do so, subject to the following conditions:
+** 
+** The above copyright notice and this permission notice shall be included in
+** all copies or substantial portions of the Software.
+** 
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+** THE SOFTWARE.
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <boost/program_options.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include <boost/tokenizer.hpp>
+
+using namespace std;
+namespace po = boost::program_options;
+
+int main(int argc, char **argv, char **ppenv) {
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help", "produce help message")
+		("directory", po::value<string>(), "The directory to check for new torrents in")
+	;
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+
+	return 0;
+}
